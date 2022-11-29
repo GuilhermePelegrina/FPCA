d9027d15f90cfbc336692ef2cff177c9e7fd8ff5
GuilhermePelegrina <46330260+GuilhermePelegrina@users.noreply.github.com>
2022-11-29 11:38:25 +0100

    Update creditProcess_mono_eq.m

--- creditProcess_mono_eq.m
+++ creditProcess_mono_eq.m
@@ -14,7 +14,7 @@ sensitive = data(:,1);
 normalized = (sensitive-1).*(sensitive-2);
 
 % Extract the sensitive attribute from the dataset
-data = data(:,2:end-1);
+data = data(:,[3,5:end-1]);
 
 % Data for low educated populattion
 data_lowEd = data(find(normalized),:);
